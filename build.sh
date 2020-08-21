4346c495b21864e864fe5ce81cddac3848b5d501
Andrew Cowie <istathar@gmail.com>
2020-08-21 19:57:24 +1000

    Switch to podman

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker pull docker.io/library/fedora:29
+podman pull docker.io/library/fedora:29
 
-docker build \
+podman build \
 	--tag="docker.io/oprdyn/fedora:29" \
 	--network="proxy" \
 	.
